6bc875a23e32b4f6e25439807b0d220044f7ab1b
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-09-11 21:06:32 +0300

    Renamed the app name

--- config/web.php
+++ config/web.php
@@ -11,7 +11,7 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
-    'name' => 'Diary 6',
+    'name' => 'Diary - the Six Time Book',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
