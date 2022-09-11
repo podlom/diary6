53a51bc7176c57fada07f9e6d7ed76340a9c1db5
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-09-11 22:11:37 +0300

    Improved /site/good-seeds template

--- views/site/good-seeds.php
+++ views/site/good-seeds.php
@@ -5,11 +5,20 @@
 
 use yii\helpers\Html;
 
-$this->title = 'Good seeds plated';
+$this->title = 'Good seeds planted';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about">
+<div class="site-good-seeds">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <code><?= var_export($seedsData, true) ?></code>
+    <table class="table-responsive">
+        <tr><th>Virtue #</th><th>Virtue Name</th><th>Count Good Seeds Planted</th></tr>
+    <?php
+    $htmlData = '';
+    foreach ($seedsData as $s1) {
+        $htmlData .= '<tr><td>' . $s1['virtue_id'] . '</td><td>' . $s1['name'] . '</td><td>' . $s1['cnt_good_seeds_planted'] . '</td></tr>';
+    }
+    ?>
+    </table>
+
 </div>
