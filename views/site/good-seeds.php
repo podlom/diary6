7d3330a9dada952322d50b4934a23cc720e40388
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-09-16 23:48:44 +0300

    Improved /site/good-seeds view

--- views/site/good-seeds.php
+++ views/site/good-seeds.php
@@ -22,12 +22,15 @@ $this->title = 'Good seeds planted: ' . $numGoodSeeds;
         <tr><th>ID #</th><th>Virtue Name</th><th>Count Good Seeds Planted</th></tr>
     <?php
         $htmlData = '';
-        foreach ($virtue as $v1) {
+        foreach ($virtue as $v1 => $vName) {
             $cntSeedsPlanted = 0;
-            if ($v1['id'] == $seedsData['virtue_id']) {
-                $cntSeedsPlanted = $seedsData['cnt_good_seeds_planted'];
+            foreach ($seedsData as $s1) {
+                if (isset($s1['virtue_id']) && ($s1['virtue_id'] == $v1)) {
+                    $cntSeedsPlanted = $s1['cnt_good_seeds_planted'];
+                    break;
+                }
             }
-            $htmlData .= '<tr><td class="text-center">' . $v1['id'] . '</td><td>' . $v1['name'] . '</td><td class="text-center font-weight-bold">' . $cntSeedsPlanted . '</td></tr>';
+            $htmlData .= '<tr><td class="text-center">' . $v1 . '</td><td>' . $vName . '</td><td class="text-center font-weight-bold">' . $cntSeedsPlanted . '</td></tr>';
         }
         echo $htmlData;
     ?>
