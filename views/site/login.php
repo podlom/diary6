e69da13c47a5014512f46f3f85d15210a849b944
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-09-20 15:02:31 +0300

    Replaced test users info with a text info and a signup link

--- views/site/login.php
+++ views/site/login.php
@@ -41,7 +41,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php ActiveForm::end(); ?>
 
     <div class="col-lg-offset-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
+        If you are first time here, go and <a href="/site/login">register a new account</a>.
     </div>
 </div>
