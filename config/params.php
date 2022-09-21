df242a326e884e4071cdda7cc9fffb831e44b594
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-09-21 23:41:26 +0300

    Fix params config/params.php

--- config/params.php
+++ config/params.php
@@ -7,4 +7,5 @@ return [
     'senderName' => 'Diary - The Six Time Book mailer',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
+    'bsVersion' => '5.x', // this will set globally `bsVersion` to Bootstrap 5.x for all Krajee Extensions
 ];
