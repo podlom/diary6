be670eb09532be79ff0d4d4d9a12b28c69380b3a
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-09-21 23:36:12 +0300

    Changed is_positive checkbox to kartik-v/yii2-widget-switchinput

--- views/site/seed.php
+++ views/site/seed.php
@@ -1,9 +1,5 @@
 <?php
 
-use yii\helpers\Html;
-use yii\widgets\ActiveForm;
-
-
 /* @var $this yii\web\View */
 /* @var $model app\models\Seed */
 /* @var $form ActiveForm */
@@ -12,18 +8,23 @@ use yii\widgets\ActiveForm;
 /* @var $date string */
 /* @var $userId int */
 
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use kartik\switchinput\SwitchInput;
+
+
 ?>
 <div class="site-seed">
 
     <?php $form = ActiveForm::begin(); ?>
 
+        <?php echo SwitchInput::widget(['name' => 'is_positive', 'type' => SwitchInput::CHECKBOX ]); ?>
         <?= $form->field($model, 'virtue_id')->dropDownList($virtue, ['prompt' => '--- Choose virtue ---']) ?>
         <?= $form->field($model, 'seed_time')->textInput(['value' => $time]) ?>
         <?= $form->field($model, 'seed_date')->textInput(['value' => $date])  ?>
         <?= $form->field($model, 'user_id')->hiddenInput(['value' => $userId])->label(false) ?>
         <?= $form->field($model, 'description')->textarea([]) ?>
         <?= $form->field($model, 'added_at')->hiddenInput(['value' => $date . ' ' . $time])->label(false) ?>
-        <?= $form->field($model, 'is_positive')->checkbox([]) ?>
     
         <div class="form-group">
             <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-primary']) ?>
