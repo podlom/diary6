082e0253a23df27778febe5302f8bbccfbeba3c6
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-09-24 22:57:30 +0300

    Do not allow not logged in user access

--- config/web.php
+++ config/web.php
@@ -52,6 +52,19 @@ $config = [
             ],
         ],
     ],
+    'as beforeRequest' => [ // if guest user access site so, redirect to login page.
+        'class' => 'yii\filters\AccessControl',
+        'rules' => [
+            [
+                'actions' => ['login', 'error'],
+                'allow' => true,
+            ],
+            [
+                'allow' => true,
+                'roles' => ['@'],
+            ],
+        ],
+    ],
     'params' => $params,
     'modules' => [
         'utility' => [
